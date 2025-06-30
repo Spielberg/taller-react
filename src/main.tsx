34b8c5242a01593d38568d67145d2a3b26b23469
Fco Javier Sánchez Ostiz <javier.sanchez@jakala.com>
2025-06-30 15:41:17 +0200

    Elimina una línea redundante en main.tsx al importar el componente App, manteniendo la estructura del archivo. Se mejora la claridad del código.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import App from './App.tsx'
 
 const queryClient = new QueryClient()
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
